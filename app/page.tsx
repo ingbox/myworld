3e0e063c9b5d43ca8ac98ca618760e19e85d3088
ingbox01 <ingbox01@gmail.com>
2026-05-06 11:46:49 +0900

    ci: test 11

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      CICD Test v10
+      CICD Test v11
 
     </div>
   );
