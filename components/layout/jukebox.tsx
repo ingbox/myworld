d6c5e65ba865695d05d62191994a594ac4ef4d9f
ingbox01 <ingbox01@gmail.com>
2026-05-27 23:14:50 +0900

    feat: 주크박스 구현 - 제목 나오도록 표시  및 및 예외처리

--- components/layout/jukebox.tsx
+++ components/layout/jukebox.tsx
@@ -18,9 +18,9 @@ export default function Jukebox() {
     const { queue, currentIndex, playNext, playPrev } = usePlayerStore()
 
     const togglePlayButton = () => {
-        if(!audioRef.current) return;
+        if (!audioRef.current) return;
 
-        if(isPlaying) {
+        if (isPlaying) {
             audioRef.current.pause();
         } else {
             audioRef.current.play();
@@ -30,28 +30,43 @@ export default function Jukebox() {
 
     useEffect(() => {
         const audio = audioRef.current;
-        if(!audio) return;
+        if (!audio) return;
 
         const updateTime = () => {
             setCurrentTime(audio.currentTime);
             setDuration(audio.duration | 0)
         }
 
+        const handleEnded = () => {
+            if(queue[currentIndex + 1] == undefined) {
+                setIsPlaying(false);
+            } else {
+                playNext();
+            }
+        };
+
         audio.addEventListener("timeupdate", updateTime);
         audio.addEventListener("loadedmetadata", updateTime);
-    },[]);
+        audio.addEventListener("ended", handleEnded);
+
+        return () => {
+            audio.removeEventListener("timeupdate", updateTime);
+            audio.removeEventListener("loadedmetadata", updateTime);
+            audio.removeEventListener("ended", handleEnded);
+        };
+    }, [queue, playNext]);
 
     useEffect(() => {
         if (!audioRef.current) return;
         if (!queue[currentIndex]) return;
-    
+
         audioRef.current.src = queue[currentIndex].download_url;
         audioRef.current.play();
-    
+
         setIsPlaying(true);
     }, [currentIndex, queue]);
 
-    const formatTime = (time : number) => {
+    const formatTime = (time: number) => {
         const minutes = Math.floor(time / 60);
         const seconds = Math.floor(time % 60).toString().padStart(2, "0");
 
@@ -61,7 +76,7 @@ export default function Jukebox() {
     const handleSeek = (e: React.ChangeEvent<HTMLInputElement>) => {
         const newTime = parseFloat(e.target.value);
 
-        if(audioRef.current) {
+        if (audioRef.current) {
             audioRef.current.currentTime = newTime;
             setCurrentTime(newTime);
         }
@@ -76,26 +91,26 @@ export default function Jukebox() {
             playPrev();
         }
     };
-    
+
     const handleVolumeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const vol = parseInt(e.target.value);
         setVolume(vol);
-        if(audioRef.current) {
+        if (audioRef.current) {
             audioRef.current.volume = vol / 100;
         }
     }
 
     const toggleMute = () => {
-        if(volume === 0) {
+        if (volume === 0) {
             setVolume(previousVolume);
-            if(audioRef.current) {
+            if (audioRef.current) {
                 audioRef.current.volume = previousVolume / 100;
-            } 
+            }
         } else {
 
             setPreviousVolume(volume);
             setVolume(0);
-            if(audioRef.current) {
+            if (audioRef.current) {
                 audioRef.current.volume = 0;
             }
         }
@@ -106,88 +121,98 @@ export default function Jukebox() {
             {/* 오디오 플레이어 */}
             <audio src={queue[currentIndex]?.download_url} ref={audioRef}></audio>
             <div className="w-full bg-[#eeeeee] mt-2 rounded-sm p-1">
-                <div 
-                    onClick={()=> setIsOpen(prev => !prev)}
-                    className="flex items-center h-4 bg-white rounded-xs p-2 cursor-pointer"
+                <div
+                    onClick={() => setIsOpen(prev => !prev)}
+                    className="flex items-center h-4 bg-white rounded-xs px-1 py-2 cursor-pointer mb-1"
                 >
-                    <Image src="/images/jukebox/cd.png" width={12} height={12} alt=""/>
-                </div>
-              {
-                isOpen && (
-                <div>
-                    <div className="flex items-center justify-center gap-2">
-                        <span className="text-xs text-gray-400">{formatTime(currentTime)}</span>
-                        <input 
-                                onChange={handleSeek}
-                                value={currentTime}
-                                type="range" 
-                                min="0" 
-                                max={duration || 0}
-                                style={{
-                                    background: `linear-gradient(to right, #2563eb ${progress}%, #cccccc ${progress}%)`
-                                  }}
-                                className="jukebox-range w-full outline-none h-[2px] bg-zinc-700 appearance-none" 
-                        />
-                        <span className="text-xs text-gray-400">{formatTime(duration)}</span>
-                    </div>
-                {queue.length === 0 ? (
-                    <div className="text-xs text-gray-400">재생 목록이 비어있음</div>
-                ) : (
-                    queue.map((item, idx) => (
+                    <Image src="/images/jukebox/cd.png" width={12} height={12} alt="" />
+                    <div className="ml-2 w-42 overflow-hidden">
                         <div
-                            key={idx}
-                            className=""
-                        >
-                            {item.title ?? `track ${idx}`}
+                        key={queue[currentIndex]?.id}
+                        className="text-xs text-gray-500 marquee whitespace-nowrap">
+                            {queue[currentIndex]?.title}
                         </div>
-                    ))
-                )}
-              
+                    </div>
                 </div>
-                )
-            }
+                {
+                    isOpen && (
+                        <div>
+                            <div className="flex items-center justify-center gap-2">
+                                <span className="text-xs text-gray-400">{formatTime(currentTime)}</span>
+                                <input
+                                    onChange={handleSeek}
+                                    value={currentTime}
+                                    type="range"
+                                    min="0"
+                                    max={duration || 0}
+                                    style={{
+                                        background: `linear-gradient(to right, #2563eb ${progress}%, #cccccc ${progress}%)`
+                                    }}
+                                    className="jukebox-range w-full outline-none h-[2px] bg-zinc-700 appearance-none"
+                                />
+                                <span className="text-xs text-gray-400">{formatTime(duration)}</span>
+                            </div>
+                            {queue.length === 0 ? (
+                                <div className="text-xs text-gray-400">재생 목록이 비어있음</div>
+                            ) : (
+                                queue.map((item, idx) => (
+                                    <div
+                                        key={idx}
+                                        className="text-xs text-gray-500 truncate"
+                                    >
+                                        {item.title ?? `track ${idx}`}
+                                    </div>
+                                ))
+                            )}
+
+                        </div>
+                    )
+                }
 
                 <div className="flex justify-between">
                     {/* 플레이 조절 */}
                     <div className="flex gap-2">
-                        <Image 
-                        onClick={handlePrev}
-                        src="/images/jukebox/skipback.svg" width={10} height={10} alt="" />
-                        <Image 
-                        onClick={togglePlayButton}
-                        src={isPlaying ? '/images/jukebox/stop.svg':'/images/jukebox/play.svg'} width={8} height={8} alt="" />
-                        <Image 
-                        onClick={playNext}
-                        src="/images/jukebox/skipforward.svg" width={10} height={10} alt="" />
+                        <Image
+                            onClick={handlePrev}
+                            className="cursor-pointer"
+                            src="/images/jukebox/skipback.svg" width={10} height={10} alt="" />
+                        <Image
+                            onClick={togglePlayButton}
+                            className="cursor-pointer"
+                            src={isPlaying ? '/images/jukebox/stop.svg' : '/images/jukebox/play.svg'} width={8} height={8} alt="" />
+                        <Image
+                            onClick={playNext}
+                            className="cursor-pointer"
+                            src="/images/jukebox/skipforward.svg" width={10} height={10} alt="" />
                     </div>
                     {/* 음량 조절 */}
                     <div className="flex items-center gap-2">
                         {
-                            volume === 0 ? 
-                            <Image
-                                onClick={toggleMute}               
-                                src="/images/jukebox/volume-off.svg" 
-                                width={12} 
-                                height={12} 
-                                alt="" 
-                            />
+                            volume === 0 ?
+                                <Image
+                                    onClick={toggleMute}
+                                    src="/images/jukebox/volume-off.svg"
+                                    width={12}
+                                    height={12}
+                                    alt=""
+                                />
                                 :
-                            <Image
-                                onClick={toggleMute}
-                                src="/images/jukebox/volume.svg" 
-                                width={12} 
-                                height={12} 
-                                alt="" 
-                            />
+                                <Image
+                                    onClick={toggleMute}
+                                    src="/images/jukebox/volume.svg"
+                                    width={12}
+                                    height={12}
+                                    alt=""
+                                />
                         }
-                        <input 
+                        <input
                             onChange={handleVolumeChange}
                             value={volume}
-                            type="range" 
-                            min="0" 
-                            max="100" 
-                            className="w-[50px] outline-none h-[2px] bg-zinc-700 accent-white appearance-none"/>
-                          {/* 주크박스 페이지 이동 */}
+                            type="range"
+                            min="0"
+                            max="100"
+                            className="w-[50px] outline-none h-[2px] bg-zinc-700 accent-white appearance-none" />
+                        {/* 주크박스 페이지 이동 */}
                         <div>
 
                         </div>
