79792a542f936381b2c838f3a1250a64f163ddae
ingbox01 <ingbox01@gmail.com>
2026-06-06 20:33:29 +0900

    error: proxy 재추가

---
+++ proxy.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export function proxy(request: NextRequest) {
+    // auth로 인증 불러와서 실행해도 돼 토큰 검증이나 이런 것들
+
+    // 메인 화면 > 싸이 화면
+    if(request.nextUrl.pathname === '/' || request.nextUrl.pathname === '/cy') {
+        return NextResponse.redirect(new URL('/cy/home', request.url));
+    }
+
+    if(request.nextUrl.pathname === '/admin') {
+        return NextResponse.redirect(new URL('/admin/home', request.url));
+    }
+
+    return NextResponse.next();
+}
