3024fedf7dfc43a17a69197147c8ad65e666a673
ingbox01 <ingbox01@gmail.com>
2026-07-09 13:23:33 +0900

    fix: API 이름 수정

--- app/admin/(photo)/photo/page.tsx
+++ app/admin/(photo)/photo/page.tsx
@@ -1,11 +1,11 @@
-import { getTypeList } from "@/app/actions/common/photo";
+import { getPhotoTypeList } from "@/app/actions/common/photo";
 import Editor from "@/components/admin/photo/Editor";
 import AdminNavigation from "@/components/layout/AdminNavigation";
 
 export default async function Page() {
 
    // 카테고리 가져오기
-   const typeList = await getTypeList();
+   const typeList = await getPhotoTypeList();
 
   return (
     <div>
